669ce03c0b70b55a40a476667dd74ed993516d36
Necroliner <noe.boyere@gmail.com>
2024-04-08 14:19:40 +0200

    initialisation du projet

---
+++ bootcamp-helpcenter/src/main/java/com/example/necroliner/bootcamphelpcenter/BootcampHelpcenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.necroliner.bootcamphelpcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampHelpcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampHelpcenterApplication.class, args);
+	}
+
+}
---
+++ bootcamp-helpcenter/src/test/java/com/example/necroliner/bootcamphelpcenter/BootcampHelpcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.necroliner.bootcamphelpcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampHelpcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
