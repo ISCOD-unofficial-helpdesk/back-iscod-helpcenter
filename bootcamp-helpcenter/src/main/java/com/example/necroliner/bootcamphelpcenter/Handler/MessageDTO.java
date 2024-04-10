6c262e0204c7be57c19a3d8c9710e13d5a285ad8
Necroliner <noe.boyere@gmail.com>
2024-04-10 15:46:26 +0200

    websocket fonctionnel

---
+++ bootcamp-helpcenter/src/main/java/com/example/necroliner/bootcamphelpcenter/Handler/MessageDTO.java
@@ -0,0 +1,33 @@
+package com.example.necroliner.bootcamphelpcenter.Handler;
+
+public class MessageDTO {
+    private Integer id;
+    private String username;
+    private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public MessageDTO() {}
+}
