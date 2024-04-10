9ac5dff52a1aa6012eddcfdce5b45c6489be8925
Necroliner <noe.boyere@gmail.com>
2024-04-10 09:11:47 +0200

    corrections entitées base de données & début websocket

--- bootcamp-helpcenter/src/main/java/com/example/necroliner/bootcamphelpcenter/WebSocketConfig.java
+++ bootcamp-helpcenter/src/main/java/com/example/necroliner/bootcamphelpcenter/WebSocketConfig.java
@@ -1,28 +1,20 @@
 package com.example.necroliner.bootcamphelpcenter;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import com.example.necroliner.bootcamphelpcenter.Handler.MessageHandler;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Endpoint pour démarrer le websocket et la connection entre les pairs
-        registry.addEndpoint("/chat").withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Message Broker sert à router les messages 
-        // Du client au helper : message
-        // Du helper vers le client: reponse
-        registry.enableSimpleBroker("/message", "/reponse");
-        // Prefix pour envoyer des messages depuis le client
-        registry.setApplicationDestinationPrefixes("/app");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new MessageHandler(), "/chat")
+            .addInterceptors(new HttpSessionHandshakeInterceptor());
     }
 }
